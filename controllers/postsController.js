18189bf48a91e54689ca1dbc555b775bf8dd91ce
Baron Hartono <baronhartono@gmail.com>
2019-08-09 11:19:45 +0700

    commit

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,15 +1,58 @@
 const conn = require('../database')
+const { uploader } = require('../helpers/uploader')
 
 module.exports = {
     getPosts: (req,res) => {
-        var sql = `Select * from posts;`;
+        var sql = `Select * from tari;`;
         conn.query(sql, (err,result) => {
-            if(err) res.status(500).send({ message: 'Error!', error: err})
+            if(err) return res.status(500).send({ message: 'Error!', error: err})
 
-            res.status(200).send(result)
+            return res.status(200).send(result)
         })
     },
     addPost: (req,res) => {
-
+        try {
+            const path = '/post/images'; //file save path
+            const upload = uploader(path, 'POS').fields([{ name: 'image'}]); //uploader(path, 'default prefix')
+    
+            upload(req, res, (err) => {
+                if(err){
+                    return res.status(500).json({ message: 'Upload picture failed !', error: err.message });
+                }
+    
+                const { image } = req.files;
+                console.log(image)
+                const imagePath = image ? path + '/' + image[0].filename : null;
+                console.log(imagePath)
+    
+                console.log(req.body.data)
+                const data = JSON.parse(req.body.data);
+                console.log(data)
+                data.image = imagePath;
+                
+                var sql = 'INSERT INTO posts SET ?';
+                conn.query(sql, data, (err, results) => {
+                    if(err) {
+                        console.log(err.message)
+                        fs.unlinkSync('./public' + imagePath);
+                        return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message });
+                    }
+                   
+                    console.log(results);
+                    sql = 'SELECT * from posts;';
+                    conn.query(sql, (err, results) => {
+                        if(err) {
+                            console.log(err.message);
+                            return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message });
+                        }
+                        console.log(results);
+                        
+                        res.send(results);
+                    })   
+                })    
+            })
+        } catch(err) {
+            return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message });
+        }
     }
 }
